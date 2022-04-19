2f91a914fa6cef04a99142aa587839bdd7aa6ba5
puercalvaro <64426866+puercalvaro@users.noreply.github.com>
2022-04-19 18:58:52 -0400

    inmo

---
+++ src/index.js
@@ -0,0 +1,134 @@
+require("dotenv").config();
+const { Client, MessageEmbed, Message, CommandInteractionOptionResolver} = require("discord.js");
+const client = new Client({ intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_BANS","GUILD_EMOJIS_AND_STICKERS", "GUILD_INTEGRATIONS",
+"GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING"] });
+client.manager = require("../src/manager")(client);
+const {token, clientId, guildId} = require('../config.json');
+const { setTimeout } = require("timers");
+const { REST } = require('@discordjs/rest');
+const { Routes, ApplicationCommandOptionType } = require('discord-api-types/v9');
+const youtubeSuggest = require('youtube-suggest')
+
+
+const commands = [{
+  name: 'play',
+  description: 'reproduce música.',
+  options: [{
+    name: 'song',
+    description: 'buscar canción.',
+    required: true,
+    autocomplete: true,
+    type: ApplicationCommandOptionType.String,
+  }]
+},
+  {
+    name: 'skip',
+    description: 'Skipear canción',
+  }
+]; 
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.once("ready", () => {
+  console.log("Discord Bot has logged in.");
+  client.manager.init(client.user.id);
+})
+
+client.login(token);
+
+client.on("raw", (d) => client.manager.updateVoiceState(d));
+
+client.on('interactionCreate', async interaction => {
+
+  //Autocomplete suggestions
+  if (interaction.isAutocomplete && interaction.type === 'APPLICATION_COMMAND_AUTOCOMPLETE') { // 'APPLICATION_COMMAND'' isAutocomplete entra igual y crashea.
+
+    if(!interaction.options.getString('song')) return;
+    //console.log(interaction)
+    //console.log('-----------------------------')
+    youtubeSuggest(interaction.options.getString('song')).then(function (results) { //YT Suggestions
+    //assert(Array.isArray(results))
+    //assert(typeof results[0] === 'string')
+    for (let i = 0; i < results.length; i++) { //Hace que no crashee por 'Invalid body form'
+      if(typeof results[i] !== 'string') {
+        if (i == results.length - 1) {
+          interaction.respond([{
+            name: 'sin resultados',
+            value: ' '
+          }])
+        }
+        return;
+      }
+    }
+    const respond = []
+    for (let i = 0; i < results.length; i++) { // Suggests
+      respond.push({name: results[i], value: results[i]})
+    }
+    interaction.respond(respond)
+    })
+  }
+  //return si no es comando
+  if (!interaction.isCommand()) return;
+  ///play canciones.
+  if (interaction.commandName === 'play') {
+    try {
+      const res = await client.manager.search(
+        interaction.options.getString('song'),
+        interaction.guild.members.cache.get(interaction.user.id).user.username
+      )
+      const player = client.manager.create({
+          guild: interaction.guild.id,
+          voiceChannel: interaction.guild.members.cache.get(interaction.user.id).voice.channel.id,
+          //voiceChannel: message.guild.channels.cache.find(r => r.id === "812476431170535424").id,
+          textChannel: interaction.channel.id,
+      });
+        client.manager.init(client.user.id);
+        player.queue.add(res.tracks[0]);
+        if (!player.playing && !player.paused /*&& !player.queue.length*/) {player.connect(); player.play() }
+        interaction.reply(`${interaction.guild.members.cache.get(interaction.user.id).displayName} ha ponío [${res.tracks[0].title}](${res.tracks[0].uri})`)
+        console.log(`${interaction.guild.members.cache.get(interaction.user.id).displayName} ha ponío ${res.tracks[0].title}`)
+      } catch (error) {
+        console.log(error)
+      }
+  }
+  // skipea la canción
+  if (interaction.commandName === 'skip') {
+    const player = client.manager.players.get(interaction.guild.id);
+    if(player) {
+      player.stop();
+    }
+    interaction.reply(`${interaction.guild.members.cache.get(interaction.user.id).displayName} skipeó la canción`)
+    console.log(`${interaction.guild.members.cache.get(interaction.user.id).displayName} skipeó la canción`)
+  }
+
+});
+
+//Conecta al voice
+client.on('voiceStateUpdate', (oldState, newState) => {
+  // check for bot
+  if (oldState.member.user.bot) {
+    if (oldState.member.id === "547905866255433758" || oldState.member.id === "201503408652419073") {
+      if (oldState.member.voice.channel != null) {
+        console.log(`${oldState.member.displayName} se ha conectado.`)
+        setTimeout(() => oldState.member.voice.disconnect(), 1000)
+        console.log(`${oldState.member.displayName} ha sido desconectado.`)
+      }
+    }
+  }
+  // the rest of your code
+})
---
+++ src/manager.js
@@ -0,0 +1,92 @@
+const { Manager } = require("erela.js");
+const {MessageEmbed} = require("discord.js");
+var embedAux = null
+
+module.exports = function (client) {
+  return new Manager({
+    nodes: [
+      {
+        //host: "lavalink-node.herokuapp.com",
+        //host: "lavalink-graljadue.herokuapp.com",
+        host: "localhost",
+        port: 80,
+        password: "pichulajadue",
+      },
+    ],
+    send(id, payload) {
+      const guild = client.guilds.cache.get(id);
+      if (guild) guild.shard.send(payload);
+    },
+  })
+    .on("nodeConnect", (node) =>{ 
+      console.log(`Node ${node.options.identifier} connected`) 
+    }
+    )
+
+    .on("nodeError", (node, error) =>
+      console.log(
+        `Node ${node.options.identifier} had an error: ${error.message}`
+      )
+    )
+    
+    .on("trackStart", async (player, track)  => {
+      if (embedAux !== null) embedAux.delete()
+      createEmbed(player, track)
+        console.log(`Estás escuchando: [${track.title}]`)
+    })
+    
+    .on("queueEnd", (player) => {
+      client.channels.cache.get(player.textChannel);//.send("Queue has ended.");
+      console.log("Queue has ended.");
+      setTimeout(() => player.destroy(), 30000)
+    });
+
+    function createEmbed(player, track) {
+      
+      const filter = (reaction, user) => { //agrega los emoji de numeros al filter
+        //return '🔁' && client.channels.cache.get(player.textChannel).members.includes(user.id);
+        return '🔁' && user.id != '844403422894882846';
+      };
+      const embed = new MessageEmbed()
+          const trackname = track.title
+          var durSec = Math.trunc(track.duration/1000%60-1);
+          if (durSec < 0) durSec = 0
+          embed.setTitle(':arrow_forward:  Estás escuchando  :musical_note: :sweat_drops:')
+          embed.setDescription(`[${track.title}](${track.uri}) :two_hearts:`)
+          embed.setFooter({text:`${0}:${0}${0}/${0}:${Math.trunc(durSec/10)}${Math.trunc(durSec%10)}`})
+          embed.setColor("DARK_RED");
+          client.channels.cache.get(player.textChannel).send({ embeds: [embed] })
+          .then(sentEmbed => {
+            var aux = -1
+            var trackname = track.title
+            timeline(track.title)
+            function timeline (trackname) {
+                var posMin = Math.trunc(player.position/1000/60);
+                var posSec = Math.trunc(player.position/1000%60 - 1);
+                if (posSec < 0) posSec = 0
+                var durMin = Math.trunc(track.duration/1000/60);
+                var durSec = Math.trunc(track.duration/1000%60-1);
+                if (durSec < 0) durSec = 0
+                aux = posSec
+                embed.setTitle(':arrow_forward:  Estás escuchando  :musical_note: :sweat_drops:')
+                embed.setDescription(`[${track.title}](${track.uri}) :two_hearts:`)
+                embed.setFooter({text: `${Math.trunc(posMin)}:${Math.trunc(posSec/10)}${Math.trunc(posSec%10)}/${Math.trunc(durMin)}:${Math.trunc(durSec/10)}${Math.trunc(durSec%10)}`})
+                embed.setColor("DARK_RED");
+                sentEmbed.edit({ embeds: [embed] })
+                .then(sentEmbed.react("🔁"))
+                  embedAux = sentEmbed
+                  const collector = sentEmbed.awaitReactions({ filter, max: 1, time: track.duration, errors: ['time'] })
+                  .then(collected => {
+                    const reaction = collected.first();
+                    var tracknameaux = trackname
+                    if (reaction.emoji.name === "🔁") {
+                      sentEmbed.reactions.removeAll()
+                      return timeline(track.title)
+                  }
+                  }).catch(collected =>{
+                  })
+            }
+          })
+    }
+};
+
